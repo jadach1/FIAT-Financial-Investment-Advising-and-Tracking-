46714fc16f7f747aea93658ea432446c9ffb34c2
bblarney <93brett@gmail.com>
2019-01-29 20:27:38 -0500

    added server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+// Required Modules
+var express = require("express");
+
+var app = express()
+
+var port = process.env.PORT || 10002
+
+const hostname ='10.10.193.143'
+
+app.use(express.static("../dist/FIAT/"));
+
+app.get("/", function(req, res) {
+    res.sendFile("./index.html"); //index.html file of your angularjs application
+});
+
+// Start Server
+app.listen(port, function () {
+    console.log( "Express server listening on port " + port);
+});
