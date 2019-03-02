f6e636422d8c84e2ddbfd66f128c110538df6463
Filpe Ferreira <fcde-souza-ferreira@myseneca.ca>
2019-03-02 10:52:55 -0500

    user-profile html and component

--- src/app/component/user-profile/user-profile.component.ts
+++ src/app/component/user-profile/user-profile.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { User } from '../../model/user';
+import { UserService } from '../../service/user.service';
+import { FormControl } from '@angular/forms';
+import { AuthenticationService } from '../../service/authentication.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,7 +13,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  public user: User;
+  submitted = false;
+
+  constructor(private router: Router, private authService: AuthenticationService,
+    private userService: UserService) {
+    this.user = new User();
+    this.userService.currentUser().subscribe(
+      res => this.user = res
+    );
+  }
+
+  updateUser() {
+    console.log(
+      this.user.firstname + ',',
+      this.user.lastname + ',',
+      this.user.email + ',',
+      this.user.password + ','
+    );
+    this.submitted = true;
+    this.save();
+    this.router.navigateByUrl('/');
+  }
+
+  private save(): void {
+    console.log('saving user');
+    this.userService.updateUser(this.user)
+        .subscribe();
+  }
 
   ngOnInit() {
   }
