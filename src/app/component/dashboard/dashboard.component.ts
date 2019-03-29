5c0f9ff13591c03d3e40ce94804a80a876040861
jadach1 <jacobadach@live.ca>
2019-03-29 15:47:55 +0100

    dropdown list functionality for api

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -12,29 +12,29 @@ import { AssetService }          from  '../../service/asset.service'
 import { PortfolioService }      from '../../service/portfolio.service'
 import { User }                  from 'src/app/model/user';
 
+
+/*
+This is the used by the dashboard dropdown list,
+an asset needs to be paired with the portfolio ID,
+user needs to see the portfolio Name
+*/
 interface portfolioNames{
-  id: any,
+  id  : any,
   name: any
 }
-
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
 export class DashboardComponent implements OnInit {
 
-  public user   : User
-  pNames: portfolioNames[]
-  assets: any[]
-  portfolio: string
-
+  user    : User
+  pNames  : portfolioNames[]
+  assets  : any[]
   assetsInPortfolios = new Array()
-
-  assetsInPortfolios2: any[][]
-
- 
 
   constructor( public nav: NavbarService, 
                public sidebar: SidebarService, 
@@ -43,23 +43,35 @@ export class DashboardComponent implements OnInit {
                private PortfolioService: PortfolioService,
                private assetService: AssetService) {
 
-    this.nav.show(); 
-    this.sidebar.show();
-
-    this.user = new User();
-
-    this.userService.currentUser().subscribe(
-      res =>this.user = res,
-      err => console.log("error connecting to database"),
-      ()  => this.fetchP()
-    );
-  }
+              this.nav.show(); 
+              this.sidebar.show();
+              this.user = new User();
+              this.userService.currentUser()
+                .subscribe(
+                  res => this.user = res,
+                  err => console.log("error connecting to database"),
+                  ()  => this.fetchPortfolioNames()
+                );
+  } // constructor ends
  
   ngOnInit() {
       
   }
 
-  private fetchP(): void{
+  /*
+    https://newsapi.org/v2/everything?q=nvda&from=2019-03-27&sortBy=publishedAt&apiKey=334280a9530a40ffaf133c91f01013e6
+    need today's date in the form or yyyy-mm-dd
+    need symbol
+    need to reconstruct string
+
+    need to call news service
+    news service has an interface
+    title
+    description
+    url 
+    author
+  */
+  private fetchPortfolioNames(): void{
     this.PortfolioService.getPortfolioNames(this.user.username)
       .subscribe(
         res=> this.pNames = res,
@@ -68,9 +80,9 @@ export class DashboardComponent implements OnInit {
       )
   }
 
-  private fetchAssets(): void{
+  private fetchAssets(idNumber: any): void{
     console.log("called")
-    this.assetService.getAssetNames("1")
+    this.assetService.getAssetNames(idNumber)
     .subscribe(
       res=> this.assets = res,
       err=> console.log("error connecting to database in dashboard to get assets"),
@@ -78,16 +90,16 @@ export class DashboardComponent implements OnInit {
     )
   }
 
-//   onOptionsSelected(value:string){
-//     console.log("the selected value is " + value);
-// }
+  public selectName(event): void {
 
-  public selectName(value: any): void{
-    // let value = event.target.value;
-    // this.portfolio = value;
-    // console.log("this is " + value + " and this is " + this.portfolio);
-    console.log("this is  " + value)
-    this.fetchAssets()
-  }
+    this.pNames.forEach((element)=>{
+        if ( event === element.name)
+        {
+          console.log(element.name + " matches with " + event)
+          this.fetchAssets(element.id)
+        }
+    }) // end of for loop
+  }  // end of selectName
+
 }
 
